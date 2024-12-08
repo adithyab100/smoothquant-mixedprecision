095499981f9488e00f890178d9a0dbec1dd84a0d
Adithya Balachandran <adithyab@mit.edu>
2024-12-08 14:23:06 -0500

    create model_size function

---
+++ smoothquant/model_size.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+
+
+def get_model_size(model: nn.Module, data_width=16, salient_prop = 0, group_size=-1):
+    data_width_non_salient = data_width
+    if group_size != -1:
+        data_width_non_salient += (16 + 4) / group_size
+    data_width_salient = 16
+
+    avg_data_width = data_width_non_salient * (1 - salient_prop) + data_width_salient * salient_prop
+    num_elements = 0
+    for param in model.parameters():
+        num_elements += param.numel()
+    return num_elements * avg_data_width
