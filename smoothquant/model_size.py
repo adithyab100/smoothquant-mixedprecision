f5f7c371a6d09d3b45caac56ca68fdf90a2e1942
Adithya Balachandran <adithyab@mit.edu>
2024-12-08 14:27:23 -0500

    hopefully fix model size fn

--- smoothquant/model_size.py
+++ smoothquant/model_size.py
@@ -4,9 +4,10 @@ import torch.nn as nn
 
 def get_model_size(model: nn.Module, data_width=16, salient_prop = 0, group_size=-1):
     data_width_non_salient = data_width
+    data_width_salient = 16
     if group_size != -1:
         data_width_non_salient += (16 + 4) / group_size
-    data_width_salient = 16
+        data_width_salient += (16 + 4) / group_size
 
     avg_data_width = data_width_non_salient * (1 - salient_prop) + data_width_salient * salient_prop
     num_elements = 0
